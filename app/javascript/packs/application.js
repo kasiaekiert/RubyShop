5b8622b26fd62ad19c3beef1e1c04422f8921f2f
Oskar Lakner <haerdde@gmail.com>
2020-06-26 02:02:00 +0200

    wip

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,6 @@
+import 'bootstrap'
+import '../stylesheets/application';
+
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
@@ -12,8 +15,6 @@ require("@rails/activestorage").start()
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
-import 'bootstrap'
 
 $(document).ready(function() {
   $('[data-toggle="tooltip"]').tooltip();
