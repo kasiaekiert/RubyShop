ff7835b3d4a1b19bc11d274c1bd64d54f1d9a1d6
Kasia Ekiert <kasia.ekiert@gmail.com>
2021-09-15 19:46:13 +0200

    faker in development

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,60 +1,58 @@
 # frozen_string_literal: true
 
-module Admin
-  class AdminController < ApplicationController
-    before_action :user_admin_authenticate
+class Admin::AdminController < ApplicationController
+  before_action :user_admin_authenticate
 
-    def user_admin_authenticate
-      unless current_user.admin?
-        flash[:alert] = 'You are not authorized'
-        redirect_to root_path
-      end
+  def user_admin_authenticate
+    unless current_user.admin?
+      flash[:alert] = 'You are not authorized'
+      redirect_to root_path
     end
+  end
 
-    def dashboard
-      @categories = Category.all
-      @brands = Brand.all
-      @search = Product.ransack(params[:q])
-      @products = @search.result.joins(:category, :brand)
+  def dashboard
+    @categories = Category.all
+    @brands = Brand.all
+    @search = Product.ransack(params[:q])
+    @products = @search.result.joins(:category, :brand)
+  end
+
+  def edit; end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to admin_root_path, notice: 'Product was successfully created.'
+    else
+      render :new
     end
+  end
 
-    def edit; end
-
-    def new
-      @product = Product.new
+  def update
+    if @product.update(product_params)
+      redirect_to admin_root_path, notice: 'Product was successfully updated.'
+    else
+      render :edit
     end
+  end
 
-    def create
-      @product = Product.new(product_params)
+  def destroy
+    @product.destroy
+    redirect_to admin_root_path, notice: 'Product was successfully destroyed.'
+  end
 
-      if @product.save
-        redirect_to admin_root_path, notice: 'Product was successfully created.'
-      else
-        render :new
-      end
-    end
+  private
 
-    def update
-      if @product.update(product_params)
-        redirect_to admin_root_path, notice: 'Product was successfully updated.'
-      else
-        render :edit
-      end
-    end
+  def set_product
+    @product = Product.find(params[:id])
+  end
 
-    def destroy
-      @product.destroy
-      redirect_to admin_root_path, notice: 'Product was successfully destroyed.'
-    end
-
-    private
-
-    def set_product
-      @product = Product.find(params[:id])
-    end
-
-    def product_params
-      params.require(:product).permit(:name, :price, :image, :category_id, :brand_id)
-    end
+  def product_params
+    params.require(:product).permit(:name, :price, :image, :category_id, :brand_id)
   end
 end
